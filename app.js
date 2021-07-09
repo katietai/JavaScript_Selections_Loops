321be4f459e7ca702c533bf7ee6b0eb189fbc91a
Taiyanna <simstaiyanna@yahoo.com>
2021-07-09 19:22:10 -0400

    loops

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let x = 1;
+while(x <= 100){
+    if (x% 2==0)
+        continue;
+    }
+    console.log(x);
+    x++;
+ for(let x= 1; x<=100; x++){
+     if( x % 2 == 1){
+         console.log(x);
+     }
+ }
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for(let x =1; x <= 100; x++){
+    if( x% 3==0){
+        console.log("Fizz");
+    } else if ( x% 5 ==0){
+        console.log("Buzz");
+    } else if ( x% 3==0 && x % 5==0){
+        console.log("FizzBuzz");
+    }
+}
